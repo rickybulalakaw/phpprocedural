53df57a7c5b50c5a20787db2ac4522efb9b97ba0
Ricky V. Bulalakaw <rbulalakaw@gmail.com>
2020-06-19 10:58:53 +0800

    Added header and footer

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+include "templates/header.php";
+
+?>
+
+<p>Test format</p>
+
+<?php include "templates/footer.php"; ?>
+
---
+++ templates/footer.php
@@ -0,0 +1,7 @@
+    <div class="text-center">
+        <small>This website has been created by <a hre="https://github.com/rickybulalakaw">Ricky Bulalakaw</a>.</small>
+    </div>
+</div> <!-- To close the container-fluid div in header.php -->
+    
+</body>
+</html>
